b4aa9feaaf2ac00e281a46daef05d96e6e52f897
Hoang Cong Son <congsonhoang0810@gmail.com>
2021-01-29 11:09:28 +0700

    add draw.io, db, add bootstrap, reactrap

---
+++ my-app/db.sql
@@ -0,0 +1,7 @@
+drop database if exists newShop;
+create database newShop;
+use newShop;
+-- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'matkhau';
+-- flush privileges;
+-- create table and insert below
+-- ...
